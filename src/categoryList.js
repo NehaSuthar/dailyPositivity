9bbe04f256ad55d302494e49e13942314cbbdc24
Neha <nehasuthr@gmail.com>
2020-10-08 13:07:27 -0400

    cloned project6 git repository

---
+++ src/categoryList.js
@@ -0,0 +1,34 @@
+const categoryList=[
+     {
+          name:"twilight",
+          path:require("./assets/category-twilight.jpg"), // Photo by Hassan Maayiz on Unsplash
+          description:"a twilight image"
+     },
+     {
+          name:"nature",
+          path:require("./assets/category-forest.jpg"),
+          description:"a nature image"
+     },
+     {
+          name:"space",
+          path:require("./assets/category-space.jpg"),
+          description:"a space image"
+     },
+     {
+          name:"desert",
+          path:require("./assets/category-desert.jpg"), // Photo by Mark Eder on Unsplash
+          description:"a desert image"
+     },
+     {
+          name:"beach",
+          path:require("./assets/category-beach.jpg"),
+          description:"a beach image"
+     },
+     {
+          name:"sky",
+          path:require("./assets/category-sky.jpg"), // Photo by Anthony Cantin on Unsplash
+          description:"a sky image"
+     },
+]
+
+export default categoryList;
